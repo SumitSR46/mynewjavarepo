78d4ceddb9ef3c6b3067b0b79b424c6ff6401467
Sumit Rasal <155448725+SumitSR46@users.noreply.github.com>
2026-01-03 21:19:40 +0530

    Update myfile.java

--- myfile.java
+++ myfile.java
@@ -1,5 +1,6 @@
-public class myfile {
-  public static void main(string[] args) {
-    system.out.println("Hello World");
-  }
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
 }
+
